629c86433eab3667468686de62910de8320c1118
Sergio Torres <sdtorresl@unal.edu.co>
2024-07-20 13:29:31 -0500

    Initial repo

---
+++ src/main/java/co/edu/unisabana/exercise1/model/Archery.java
@@ -0,0 +1,32 @@
+package co.edu.unisabana.exercise1.model;
+
+import java.util.List;
+
+public class Archery extends Hero {
+    public int getArrows() {
+        return arrows;
+    }
+
+    public void setArrows(int arrows) {
+        this.arrows = arrows;
+    }
+
+    private int arrows;
+
+    protected Archery(int skillPoints, int experience, String visualAspect, String name, List<String> magicalSkills, List<String> defenses, List<String> attacks, int arrows) {
+        super(skillPoints, experience, visualAspect, name, magicalSkills, defenses, attacks);
+        this.arrows = arrows;
+    }
+
+    public void shootArrow() {
+        if (arrows > 0) {
+            System.out.println(this.getName() + " shoot 1 arrow");
+            arrows--;
+        }
+    }
+
+    @Override
+    public Hero clone() {
+        return null;
+    }
+}
